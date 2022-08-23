5884601f0649c287b587d7d2db1b5d238f327b99
Serzhan <serjan_kosmos@mail.ru>
2022-08-23 20:22:50 +0600

    update2

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-class Config:
+class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 # основной файл приложения. здесь конфигурируется фласк, сервисы, SQLAlchemy и все остальное что требуется для приложения.
 # этот файл часто является точкой входа в приложение
 
-# Пример
 from flask_restx import Api
 
 from app.config import Config
 from flask import Flask
 from app.database import db
 
-# from config import Config
-# from models import Review, Book
-# from setup_db import db
-
-#
 # функция создания основного объекта app
 from app.views.directors import director_ns
 from app.views.genres import genre_ns
 from app.views.movies import movie_ns
+from create_data import create_base
 
 
 def create_app(config_object: Config) -> Flask:
@@ -26,34 +21,18 @@ def create_app(config_object: Config) -> Flask:
     return application
 
 
-#
 # функция подключения расширений (Flask-SQLAlchemy, Flask-RESTx, ...)
 def register_extensions(application: Flask):
     db.init_app(application)
-    api = Api(app)
+    api = Api(application)
     api.add_namespace(movie_ns)
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
-    create_data(application, db)
-
-
-#
-#
-# функция
-def create_data(app, db):
-    with app.app_context():
-        db.create_all()
-
-        #         создать несколько сущностей чтобы добавить их в БД
-        #
-        with db.session.begin():
-            db.session.add_all()
-
-
-#
+    create_base()
 
 
 if __name__ == '__main__':
-    app = create_app(Config())
+    app_config = Config()
+    app = create_app(app_config)
     register_extensions(app)
     app.run()
